882078903a61a83bf22470e67bb5d055fcaa5187
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-02 17:18:29 +0200

    feat: created util class whose methods are needed by other classes

---
+++ src/main/java/com/nnamdi/gpi/drones/util/AppUtil.java
@@ -0,0 +1,32 @@
+package com.nnamdi.gpi.drones.util;
+
+import com.nnamdi.gpi.drones.exception.CustomException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+@Slf4j
+public enum AppUtil {
+    INSTANCE;
+
+    AppUtil() {
+    }
+
+    public static String generateUUIDString() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString().replace("-", "").toUpperCase();
+    }
+
+    public static boolean stringIsNullOrEmpty(String arg) {
+        if ((arg == null)) return true;
+        else return (arg.isEmpty()) || (arg.trim().isEmpty());
+    }
+
+    public static void validatePageRequest(int page, int size) {
+        log.info("about to validate  page request ");
+        if (page < 1 || size < 1) {
+            throw new CustomException("page and size must be positive and not less than 1");
+        }
+    }
+
+}
