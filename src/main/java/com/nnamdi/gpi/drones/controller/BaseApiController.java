a780d83e8a7d07c4d6dc909dad80351dd42b600e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-11 05:19:35 +0200

    chore: implemented BaseApiController with base paths to manage endpoint url

---
+++ src/main/java/com/nnamdi/gpi/drones/controller/BaseApiController.java
@@ -0,0 +1,9 @@
+package com.nnamdi.gpi.drones.controller;
+
+public abstract class BaseApiController {
+    private BaseApiController() {
+    }
+
+    public static final String BASE_API_PATH = "/api/v1/";
+    public static final String DRONE = "drone";
+}
