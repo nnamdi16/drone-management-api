3a37adbf471d5694ccc542bf290e0100cb76fd4f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-11 05:23:26 +0200

    chore: added PaginatedResponse to the return type of DroneService

--- src/main/java/com/nnamdi/gpi/drones/service/DroneService.java
+++ src/main/java/com/nnamdi/gpi/drones/service/DroneService.java
@@ -1,12 +1,11 @@
 package com.nnamdi.gpi.drones.service;
 
 import com.nnamdi.gpi.drones.dto.DroneDto;
-import com.nnamdi.gpi.drones.dto.RegisterDroneDto;
+import com.nnamdi.gpi.drones.dto.PaginatedResponse;
+import com.nnamdi.gpi.drones.request.RegisterDroneDto;
 import com.nnamdi.gpi.drones.dto.UpdateDroneDto;
 import com.nnamdi.gpi.drones.model.Drone;
 import io.smallrye.mutiny.Uni;
-
-import java.util.List;
 
 public interface DroneService {
     Uni<DroneDto> registerDrone(RegisterDroneDto drone);
@@ -14,7 +13,7 @@ public interface DroneService {
 
     Uni<Drone> getDronePosition(String id);
 
-    Uni<List<DroneDto>> getDrones(int page, int limit);
+    Uni<PaginatedResponse<DroneDto>> getDrones(int page, int limit);
 
 
 }
