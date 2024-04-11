32c904bef16607c913cb3c55ca65ae9650414c08
nnamdi16 <nwabuokeinnamdi@gmail.com>
2024-04-11 05:20:48 +0200

    chore: implemented ConstantsUtil that handles generic response messages

---
+++ src/main/java/com/nnamdi/gpi/drones/util/ConstantsUtil.java
@@ -0,0 +1,11 @@
+package com.nnamdi.gpi.drones.util;
+
+public class ConstantsUtil {
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String NOT_FOUND = "Entity not found";
+    public static final String ALREADY_EXIST = "Entity already exist";
+    public static final String SUCCESSFUL = "Request processed successfully";
+
+    private ConstantsUtil() {
+    }
+}
